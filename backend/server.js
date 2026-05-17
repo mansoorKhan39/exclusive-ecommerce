4171c02291ce08e869c0ec67e864a8482fd89e5a
mansoorKhan39 <mansooralikhan1029@gmail.com>
2026-05-17 21:03:47 +0500

    Fixed CORS for local and production frontend

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,13 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
-    credentials: true,
-  })
-);
-
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://exclusive-ecommerce-two-lemon.vercel.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/uploads', express.static('uploads'));
